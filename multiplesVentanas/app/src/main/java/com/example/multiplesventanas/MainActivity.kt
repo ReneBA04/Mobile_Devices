6f1b17a172e64a2844bbdf4c8126594f2358e7ab
ReneBA04 <142522518+ReneBA04@users.noreply.github.com>
2025-10-16 13:42:23 -0600

    Add files via upload

---
+++ multiplesVentanas/app/src/main/java/com/example/multiplesventanas/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.multiplesventanas
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var ventana2: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        ventana2 = findViewById(R.id.ventana2)
+
+    }
+
+    fun ventana2(view: View){
+        val intent = Intent(this, Ventana2Activity::class.java)
+        startActivity(intent)
+    }
+}
